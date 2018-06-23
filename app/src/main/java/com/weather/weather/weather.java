ce7a35f0edfe785f0f6c2b2146deb94e0b88c006
Shama09 <Shama_bid@mail.ru>
2018-06-24 02:07:05 +0300

    Урок2 Activity

--- app/src/main/java/com/weather/weather/weather.java
+++ app/src/main/java/com/weather/weather/weather.java
@@ -1,14 +1,14 @@
 package com.weather.weather;
 
+import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
 public class weather extends AppCompatActivity {
-    private void messege(String mess){
-        Toast.makeText(getApplicationContext(), mess, Toast.LENGTH_LONG).show();
+    private void message(String mess){
+        Toast.makeText(getApplicationContext(), mess, Toast.LENGTH_SHORT).show();
         Log.d("weatAPP", mess);
     }
 
@@ -18,62 +18,62 @@ public class weather extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         if (savedInstanceState == null)
-             mess = "Первое созданеи App - onCreate()";
+             mess ="Первое созданеи App - onCreate()";
         else mess = "Следующее созданеи App - onCreate()";
-        messege(mess);
+        message(mess);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        messege("App запустилось");
+        message("App запустилось");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        messege("Выполняюсь после полного запуска APP");
+        message("Выполняюсь после полного запуска APP");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        messege("Тут востанавливаем ресурсы");
+        message("Тут востанавливаем ресурсы");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        messege("Я на паузу");
+        message("Я на паузу");
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
-        messege("На всякий случай во время паузы сохроняю состояние App");
+        message("На всякий случай во время паузы сохроняю состояние App");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        messege("На всякий случай во время паузы сохроняю состояние App");
+        message("На всякий случай во время паузы сохроняю состояние App");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        messege("Меня решили востановить, а так же после меня запуститься onStart()");
+        message("Меня решили востановить, а так же после меня запуститься onStart()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        messege("Меня решили уничтожить((");
+        message("Меня решили уничтожить((");
     }
 
     @Override
     public void finish() {
         super.finish();
-        messege("Почемуто вызвался метод finish()");
+        message("Почемуто вызвался метод finish()");
     }
 }
